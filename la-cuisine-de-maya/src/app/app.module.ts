557e915afe6b726037f2e6c2396d8f791387c56d
Maya-Dnl <maya.dnl29@gmail.com>
2024-03-09 22:37:11 +0100

    PL-6 static html/css component part 1

--- la-cuisine-de-maya/src/app/app.module.ts
+++ la-cuisine-de-maya/src/app/app.module.ts
@@ -3,16 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { TopHeaderComponent } from './top-header/top-header.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { CardRecipeComponent } from './card-recipe/card-recipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopHeaderComponent,
+    NavBarComponent,
+    SearchBarComponent,
+    CardRecipeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ la-cuisine-de-maya/src/app/card-recipe/card-recipe.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-recipe',
+  templateUrl: './card-recipe.component.html',
+  styleUrl: './card-recipe.component.scss'
+})
+export class CardRecipeComponent {
+
+}
