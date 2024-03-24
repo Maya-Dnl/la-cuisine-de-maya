9be2dbe7d0d865bf1ac343a4ddc638b40b035bb0
Maya-Dnl <maya.dnl29@gmail.com>
2024-03-24 20:31:58 +0100

    PL-16 remove comments

--- la-cuisine-de-maya/src/app/components/card-recipe/card-recipe.component.ts
+++ la-cuisine-de-maya/src/app/components/card-recipe/card-recipe.component.ts
@@ -14,22 +14,3 @@ export class CardRecipeComponent {
   @Input({ required: true }) Recipe!: CardRecipeVM;
   
  }
-
-// export class CardRecipeComponent implements OnInit {
-
-//   public recipes: CardRecipeVM[] = []; // Tableau pour stocker toutes les recettes
-
-//   constructor(private recipeService: RecipeService) { }
-
-//   ngOnInit(): void {
-//     this.loadRecipes(); // Appel de la fonction pour charger les recettes lors de l'initialisation du composant
-//   }
-
-//   loadRecipes() {
-//     this.recipeService.GetRecipe(recipes.id)
-//   }
-
-//     this.Recipe = {
-//     Title: recipeData.title,
-//   }
-// };
